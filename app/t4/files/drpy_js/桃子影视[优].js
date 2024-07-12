b30f2732d7532c4d4181fe97de4ee340b6858e46
sylartest <147778677+sylartest@users.noreply.github.com>
2024-07-12 10:37:45 +0800

    更新 桃子影视[优].js

--- app/t4/files/drpy_js/桃子影视[优].js
+++ app/t4/files/drpy_js/桃子影视[优].js
@@ -65,7 +65,9 @@ var rule = {
     }),
     推荐: '*',
     double: false, // 推荐内容是否双层定
-    searchUrl: '/search/**----------fypage---.html',
+    detailUrl: '/detail/fyid.html',
+    searchUrl: '/index.php/ajax/suggest?mid=1&wd=**',
+    搜索: 'json:list;name;pic;en;id',
     filterable: 1,
     filter: "H4sIAAAAAAAAA+2Za08bVxCG/8t+ptKyQNLkW+73+z1VPkQVUqO2qdSklaoIicTYNSTYgAiOawihwYFEGMxFFJYa/ozP2v4XXfvMvDNug2WlJGqq/ebnnZ3dc+asz7w6+8jpdA5+9cj5tvcX56BjBueCWNzpcO7f/b5X8893v/upt3Hh/bocn6/F5utyCE5fB6kTk+H1pBIgNjRfLk1yzALHguSLsj9IMQKOVZLLNAAB5D1ZqEyMcp4F5M2Nms0tzrOAsSTGatm3PBYLapyV8S0ZZx367tSjVKDJonnmqwIxt1Wg2blaNsE3t8Cx2ptssLFEMQLkpYvB5jbnWcBEt1MmV+KJWkCBVp9LjIBj1afLEiNAXiYfTC5wngWMc/qt5BFgnNt/SIxAxlLUYyk25Q0Xjf+G8ywgbyAdVsokeb2EMZP8TiVdqAxmeTJgWdRXwdOdMA3ryowr4uvlrQkOW9DLXl0qVOf7ZdnBbS17rhRezze3oJZBYgRY2pVZiRFgGTIlM5yRsLBaKBW2oBZYYgTqpVExC2qB1UwsqBKaxZiUsA5N/5zNtfJWSf1zmNspoed2fkla46fS94u+X+v7RN+n9R7Re7TeLXq31rtE79K6J7qn9U7RO7Xuiu42F6aQCZaGVWGY/1YYyLowh0g4BOUwKYehHCHlCJSjpByFcoyUY1COk3IcyglSTkA5ScpJKKdIOQXlNCmnoZwh5QyUs6SchXKOlHNQzpNyHsoFUi5AuUjKRSiXSLkE5TIpl6FcIeUKlKukXIVyjZRrUK6Tch3KDVJuQLlJyk0ot0i5BeU2KbehuF8cIK35RQlSY8ZPy4sCbn5Rgsx6LbMmwVB6eC9M4NuXfT8ojjfFv7n38IFsSUsDJploij/4+ocfe+tjudPheHtmF9Kz1dfc2gnasQS1qbHgRd48XjcxHl+T1JYdWVw3PjYyC9h2h5YqpXe87Vpox8aY/K9iOQgQQ2EEEEslTHqFYxYwlq20iW/wWCxgHTdngim2PwSoXyweZNlNEOB5zxNhRfl5FvC80qjUjKDdHvnvrFEiHl4PT9aAdizHh9qmVhantaXa3ca0tFQTxdB1mKkZTgVHFuQfFuQjWwlPWr2nW70nrd7Trd6TVu/pVu9Jq/eaWv0B6OHP/62Fge7KfF09X1fm6+r5ujJfV8/Xlfm6er6uzNfV83VlvuHPyFI5kaX6PC1V115Zqlr/YGWunzdcC9oSDEwrSxACxre4Uy0muQVYQN5YIRjKc54F2cTjwQbbEwLZ/FfLmyPY/BugOmztNY+FADH/nVl8yTELeF5upTro8/MsIG98OljDiY8F5G1sBMl02R8LLRtnawl1WPs9NFlcBwu4x/KT6uNnnG3hk9if0NKE5gXDboDqomFflS5aB8QW5sLScsxCZCiiswknauxRY48a+6do7N171dhbNe9WnzoqsUJ1hg0BAe6Zmq+M8NcIAsRGXlYW8GnFgvSq3T+fVEemqik+tyHAPV/NmBw2aAu4Z6vPQ5O+nFcQ4Hk7I9LSCZDX4gzJFMMyrfHzLOhYflXFQkA9Z7fLf/7G9bSAvNS0SeY4z4K8RyumwGaIAPfMDQVZNjUEUpdls5NBXRqgmtpHPI/Jlco+Ds8sqE6769nJe01HmwOODMleGZLohCMyQpERciIj9F8xQj3aCEWvbvTqfi6vbt9fsJ3zwyAlAAA=",
     filter_url: "{{fl.类型}}-{{fl.地区}}-{{fl.排序}}-{{fl.剧情}}-{{fl.语言}}-{{fl.字母}}---fypage---{{fl.年份}}",
