148809164a76b496f01ce4689e94204070b9102e
晚风拂柳颜 <434857005@qq.com>
2024-07-25 12:15:37 +0800

    更新源

--- app/t4/files/drpy_js/星芽短剧[优].js
+++ app/t4/files/drpy_js/星芽短剧[优].js
@@ -8,14 +8,15 @@ var rule = {
     quickSearch: 0,
     headers: {
         'User-Agent': 'okhttp/4.10.0',
+        'Accept-Encoding': 'gzip',
         'x-app-id': '7',
-        'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjE4MjU0NjQsIlVzZXJJZCI6NTA5MDg3NDEsInJlZ2lzdGVyX3RpbWUiOiIyMDI0LTA3LTA5IDIwOjUwOjIxIiwiaXNfbW9iaWxlX2JpbmQiOmZhbHNlfQ.VllcrcGmVc8vPMJ8xOIfiq-87qmIGD1JeMRrayXMgts',
+        'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMxNzQ2MjgsIlVzZXJJZCI6NTA5MDg3NDEsInJlZ2lzdGVyX3RpbWUiOiIyMDI0LTA3LTA5IDIwOjUwOjIxIiwiaXNfbW9iaWxlX2JpbmQiOmZhbHNlfQ.lS-X8Mck6WCISgbp9wjysfIMAlWgThVmbEM4N54cup8',
         'platform': '1',
         'manufacturer': 'realme',
-        'version_name': '3.0.0.1',
+        'version_name': '3.1.0.1',
         'user_agent': 'Mozilla/5.0 (Linux; Android 9; RMX1931 Build/PQ3A.190605.05081124; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.114 Mobile Safari/537.36',
-        'dev_token': 'BqU-Xm_tyKyrlzdfLCIiSZUEPxDbJ5wsZ6ajiltu77EF0SdTdyyXi0oZRHnZEicMFF2SalGVmz1p5kc9YooTR_DkCGIttK_DQjmfvICY2mUqUPzUWn7bQl6AsaQMO57BvemcXtn9BJib1BjZ-1sx9N9k-wtJJX80_A5-PeKO0YyU*',
-        'app_version': '3.0.0.1',
+        'dev_token': 'BFdbZBGOEgG7QDt01ldOQNNfhO2F-rv4QcugZoFZm5_3DlPJEo_bSBeJ6dW2X3eKzxxKKWz3xJCM_u5PppGMqRuYPxcsVg9a-jriWiIoPZvHMSLbcbxTFuasqgTivTY3GabW1yP57LQSsJNQfKoX1BKYGHducrhb0bTwvigfn3gE*',
+        'app_version': '3.1.0.1',
         'device_platform': 'android',
         'personalized_recommend_status': '1',
         'device_type': 'RMX1931',
@@ -25,13 +26,14 @@ var rule = {
         'raw_channel': 'default',
         'oaid': '',
         'msa_oaid': '',
-        'uuid': 'randomUUID_a8f870fe-d890-4a10-9e85-05d90d41d731',
+        'uuid': 'randomUUID_8a0324bf-03c8-4789-8ef8-12d3bcff28f5',
         'device_id': '24250683a3bdb3f118dff25ba4b1cba1a',
-        'ab_id': ''
+        'ab_id': '',
+        'support_h265': '1'
     },
     timeout: 5000,
-    class_name: '剧场&热播剧&星选好剧&新剧&阳光剧场',
-    class_url: '1&2&7&3&5',
+    class_name: '剧场&热播剧&会员专享&星选好剧&新剧&阳光剧场',
+    class_url: '1&2&8&7&3&5',
     play_parse: true,
     lazy: $js.toString(() => {
         input = {url: input, parse: 0}
@@ -67,7 +69,6 @@ var rule = {
         };
     }),
     搜索: $js.toString(() => {
-        let d = [];
         let html = post(input, {body: {"text": KEY}})
         let list = JSON.parse(html).data.theater.search_data;
         list.forEach(it => {
