0367658f0c159842167e18cfac31adb929a22fe7
sanshu <linxiaoji89@gmail.com>
2024-09-16 15:25:14 +0800

    添加18个群友分享源
    添加18个群友分享源

--- app/t4/files/drpy_js/星芽短剧[优].js
+++ app/t4/files/drpy_js/星芽短剧[优].js
@@ -1,5 +1,4 @@
-var rule = {
-    类型: '影视',
+var rule = {类型: '影视',
     title: '星芽短剧[优]',
     host: 'https://app.whjzjx.cn',
     url: '/cloud/v2/theater/home_page?theater_class_id=fyclass&type=fyclass&class2_ids=0&page_num=fypage&page_size=24',
@@ -10,7 +9,7 @@ var rule = {
         'User-Agent': 'okhttp/4.10.0',
         'Accept-Encoding': 'gzip',
         'x-app-id': '7',
-        'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMxNzQ2MjgsIlVzZXJJZCI6NTA5MDg3NDEsInJlZ2lzdGVyX3RpbWUiOiIyMDI0LTA3LTA5IDIwOjUwOjIxIiwiaXNfbW9iaWxlX2JpbmQiOmZhbHNlfQ.lS-X8Mck6WCISgbp9wjysfIMAlWgThVmbEM4N54cup8',
+        "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3Mjc0MzkzODgsIlVzZXJJZCI6NDY4OTQxMDIsInJlZ2lzdGVyX3RpbWUiOiIyMDI0LTA1LTI2IDE5OjI5OjM1IiwiaXNfbW9iaWxlX2JpbmQiOmZhbHNlfQ.hYl3KjIWRYQPBY7P7pFedPda9yg_iUeYDp_yoeRrpjU",
         'platform': '1',
         'manufacturer': 'realme',
         'version_name': '3.1.0.1',
@@ -36,13 +35,17 @@ var rule = {
     class_url: '1&2&8&7&3&5',
     play_parse: true,
     lazy: $js.toString(() => {
-        input = {url: input, parse: 0}
+        input = {
+            url: input,
+            parse: 0
+        }
     }),
     double: true,
     一级: $js.toString(() => {
         let d = [];
         let html = request(input);
-        let data = JSON.parse(html).data.list;
+        let data = JSON.parse(html)
+            .data.list;
         data.forEach(it => {
             let id = 'https://app.whjzjx.cn/v2/theater_parent/detail?theater_parent_id=' + it.theater.id;
             d.push({
@@ -57,7 +60,8 @@ var rule = {
     二级: $js.toString(() => {
         let urls = [];
         let html = request(input);
-        let data = JSON.parse(html).data;
+        let data = JSON.parse(html)
+            .data;
         data.theaters.forEach(it => {
             urls.push(it.num + '$' + it.son_video_url);
         })
@@ -69,8 +73,13 @@ var rule = {
         };
     }),
     搜索: $js.toString(() => {
-        let html = post(input, {body: {"text": KEY}})
-        let list = JSON.parse(html).data.theater.search_data;
+        let html = post(input, {
+            body: {
+                "text": KEY
+            }
+        })
+        let list = JSON.parse(html)
+            .data.theater.search_data;
         list.forEach(it => {
             let id = 'https://app.whjzjx.cn/v2/theater_parent/detail?theater_parent_id=' + it.id;
             d.push({
