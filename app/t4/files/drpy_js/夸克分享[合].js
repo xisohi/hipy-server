23d3e912c4cdbdf60135e474fa37ff584f2278a4
晚风拂柳颜 <434857005@qq.com>
2024-10-14 00:38:55 +0800

    夸克请求头修改

--- app/t4/files/drpy_js/夸克分享[合].js
+++ app/t4/files/drpy_js/夸克分享[合].js
@@ -17,6 +17,7 @@ var rule = {
     headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) quark-cloud-drive/2.5.20 Chrome/100.0.4896.160 Electron/18.3.5.4-b478491100 Safari/537.36 Channel/pckk_other_ch',
         'Referer': 'http://pan.quark.cn/',
+        'Content-Type': 'application/json',
         'Cookie': ''
     },
     play_parse: true,
