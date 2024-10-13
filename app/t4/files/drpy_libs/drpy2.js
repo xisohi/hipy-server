29b9fe371076fb150b26ced177d1652116e37b60
晚风拂柳颜 <434857005@qq.com>
2024-10-13 22:41:26 +0800

    更新js

--- app/t4/files/drpy_libs/drpy2.js
+++ app/t4/files/drpy_libs/drpy2.js
@@ -279,7 +279,7 @@ function pre() {
 
 let rule = {};
 let vercode = typeof (pdfl) === 'function' ? 'drpy2.1' : 'drpy2';
-const VERSION = vercode + ' 3.9.51beta3 20241012';
+const VERSION = vercode + ' 3.9.51beta4 20241013';
 /** 已知问题记录
  * 1.影魔的jinjia2引擎不支持 {{fl}}对象直接渲染 (有能力解决的话尽量解决下，支持对象直接渲染字符串转义,如果加了|safe就不转义)[影魔牛逼，最新的文件发现这问题已经解决了]
  * Array.prototype.append = Array.prototype.push; 这种js执行后有毛病,for in 循环列表会把属性给打印出来 (这个大毛病需要重点排除一下)
@@ -3312,11 +3312,12 @@ function init(ext) {
                 console.log(`处理headers发生错误:${e.message}`);
             }
         }
+        oheaders = rule.headers || {};
+        oheaders = deepCopy(oheaders);
+        pre(); // 预处理
         // print(rule.headers);
         rule_fetch_params = {'headers': rule.headers || false, 'timeout': rule.timeout, 'encoding': rule.encoding};
-        oheaders = rule.headers || {};
         RKEY = typeof (key) !== 'undefined' && key ? key : 'drpy_' + (rule.title || rule.host);
-        pre(); // 预处理
         init_test();
     } catch (e) {
         console.log(`init_test发生错误:${e.message}`);
@@ -3538,6 +3539,100 @@ function isVideo(url) {
  */
 function getRule(key) {
     return key ? rule[key] || '' : rule
+}
+
+/**
+ * 深拷贝一个对象
+ * @param _obj
+ * @returns {any}
+ */
+function deepCopy(_obj){
+    return JSON.parse(JSON.stringify(_obj))
+}
+//正则matchAll
+function matchesAll(str, pattern, flatten) {
+  if (!pattern.global) {
+    pattern = new RegExp(pattern.source, "g" + (pattern.ignoreCase ? "i" : "") + (pattern.multiline ? "m" : ""));
+  }
+  var matches = [];
+  var match;
+  while ((match = pattern.exec(str)) !== null) {
+    matches.push(match);
+  }
+  return flatten ? matches.flat() : matches;
+}
+
+//文本扩展
+function stringUtils() {
+  Object.defineProperties(String.prototype, {
+    replaceX: {
+      value: function (regex, replacement) {
+        let matches = matchesAll(this, regex,true);
+        if (matches && matches.length > 1) {
+          const hasCaptureGroup = /\$\d/.test(replacement);
+          if (hasCaptureGroup) {
+            return this.replace(regex, (m) => m.replace(regex, replacement));
+          } else {
+            return this.replace(regex, (m, p1) => m.replace(p1, replacement));
+          }
+        }
+        return this.replace(regex, replacement);
+      },
+      configurable: true,
+      enumerable: false,
+      writable: true
+    },
+    parseX: {
+      get: function () {
+        try {
+          //console.log(typeof this);
+          return JSON.parse(this);
+        } catch (e) {
+          console.log(e.message);
+          return this.startsWith("[") ? [] : {};
+        }
+      },
+      configurable: true,
+      enumerable: false,
+    }
+  });
+}
+
+//正则裁切
+function cut(text, start, end, method, All) {
+  let result = "";
+  let c = (t, s, e) => {
+    let result = "";
+    let rs = [];
+    let results = [];
+    try {
+      let lr = new RegExp(String.raw`${s}`.toString());
+      let rr = new RegExp(String.raw`${e}`.toString());
+      const segments = t.split(lr);
+      if (segments.length < 2) return '';
+      let cutSegments = segments.slice(1).map(segment => {
+        let splitSegment = segment.split(rr);
+        //log(splitSegment)
+        return splitSegment.length < 2 ? undefined : splitSegment[0] + e;
+      }).filter(f => f);
+      //log(cutSegments.at(-1))
+      if (All) {
+        return `[${cutSegments.join(',')}]`;
+      } else {
+        return cutSegments[0];
+      }
+    } catch (e) {
+      console.log(`Error cutting text:${e.message}`);
+    }
+    return result;
+  }
+  result = c(text, start, end);
+  stringUtils();
+  if (method && typeof method === "function") {
+    result = method(result);
+  }
+  //console.log(result);
+  return result
 }
 
 function DRPY() {//导出函数
